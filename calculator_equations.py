5f5df3b022a1862f100b933180f38851ed7a5491
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-04-30 00:16:41 +0800

    In this commit, I added my class for the number that the user will input. I also inherited the parent class unto my child class of equations.

---
+++ calculator_equations.py
@@ -0,0 +1,9 @@
+class Number:
+    
+    def __init__(self, input_number):
+        self.input_number = input_number
+    
+class SimpleEquation(Number):
+    
+    def __init__(self):
+        pass
