eaddfb95236614e43db219011ae1e7e64771529d
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-04-30 17:07:53 +0800

    In this commit, I added my methods for simple equations and for the parenthesis that will prioritize the number inside the parentheses.

--- calculator_equations.py
+++ calculator_equations.py
@@ -4,6 +4,23 @@ class Number:
         self.input_number = input_number
     
 class SimpleEquation(Number):
-    
+
     def __init__(self):
+        pass
+    
+    def addition(self, number_input):
+        pass
+    
+    def subtraction(self, number_input):
+        pass
+    
+    def multiplication(self, number_input):
+        pass
+    
+    def division(self, number_input):
+        pass
+    
+class Parentheses(SimpleEquation):
+    
+    def prioritize(self):
         pass
