60c634b3d3c2bfc8c26f42186bb8435e1cca6ee8
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-05-03 15:58:52 +0800

    Finally, in this commit, I inherited the input equation which is my parent class to my child class which is the proper operands child class. I also add an elif condition where ** is going to be invalid, but only pass ^ symbol. It is also going to be invalid with double xx and XX.

--- calculator_equations.py
+++ calculator_equations.py
@@ -3,10 +3,10 @@ import re
 class InputEquation:
     
     def __init__(self, user_input):
-        self.user_input = user_input
+        self.equation = user_input
     
     def clean_spaces(self):
-        clean_user_input = self.user_input.replace(" ", "")
+        clean_user_input = self.equation.replace(" ", "")
         return clean_user_input
     
     def equation_list(self, user_input):
@@ -16,20 +16,27 @@ class InputEquation:
         equation = "".join(equation_list)
         return equation
 
-class ProperOperands:
+class ProperOperands(InputEquation):
     
-    def __init__(self, equation):
-        self.equation = equation
+    def __init__(self, user_input):
+        super().__init__(user_input)
     
     def proper_operator(self):
-        self.equation = self.equation.replace("x", "*").replace("X", "*")
-        self.equation = self.equation.replace(")(", ")*(")
-        self.equation = self.equation.replace("^", "**")
-        self.equation = re.sub(r'(\d)\(', r'\1*(', self.equation)        
-        self.equation = re.sub(r'\)(\d)', r')*\1', self.equation)
-        self.equation = re.sub(r'(\d)\(', r'\1*(', self.equation)
-        self.equation = re.sub(r'([a-zA-Z])\(', r'\1*(', self.equation)
-        return self.equation
+        if "xx" in self.equation:
+            return
+        elif "XX" in self.equation:
+            return
+        elif "**" in self.equation:
+            return
+        else:
+            self.equation = self.equation.replace("x", "*").replace("X", "*")
+            self.equation = self.equation.replace(")(", ")*(")
+            self.equation = self.equation.replace("^", "**")
+            self.equation = re.sub(r'(\d)\(', r'\1*(', self.equation)        
+            self.equation = re.sub(r'\)(\d)', r')*\1', self.equation)
+            self.equation = re.sub(r'(\d)\(', r'\1*(', self.equation)
+            self.equation = re.sub(r'([a-zA-Z])\(', r'\1*(', self.equation)
+            return self.equation
 
 class EvaluateEquation:
     
@@ -38,7 +45,7 @@ class EvaluateEquation:
     
     def evaluate_equation(self):
         try:
-            if re.search(r'[\+\-\*\/]{2,}', self.equation):
+            if re.search(r'[\+\-\/]{2,}', self.equation):
                 return "Error: Multiple Operators"
             result = eval(self.equation)
             return result
