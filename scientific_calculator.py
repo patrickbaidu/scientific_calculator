40133e853eff0404bcfab1b555b4e0c12131880c
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-05-06 18:10:00 +0800

    In this commit, I want to add new operations to my calculator since I named it a scientific calculator. In the first latter part of the program, I named the different operations I am going to put in my equations module. I also need to import the math module for these operations.

--- scientific_calculator.py
+++ scientific_calculator.py
@@ -4,7 +4,21 @@ from calculator_equations import EvaluateEquation
 from calculator_equations import Color
 
 while True:
-    
+    print(f"{Color.cyan + Color.bold + Color.underline}{"-"*12}A SCIENTIFIC CALCULATOR{"-"*12}{Color.end}")
+    print(f"{Color.bold}OPERATIONS AVAILABLE: [+, -, x, /, ^, sqrt, ln, log, e, pi, sin, cos, tan, arcsin, arccos, arctan]{Color.end}")
+    print(f"""
+        {Color.green}Please Use These Symbols Equivalent to the Operation Above: {Color.end}
+        1. + : Addition                     10. pi : Pi Constant
+        2. - : Subtraction                  11. sin : Sine Function
+        3. x or X or * : Multiplication     12. cos : Cosine Function
+        4. / : Division                     13. tan : Tangent Function
+        5. ^ : Exponent                     14. asin : Inverse Sin Function
+        6. sqrt : Square Root               15. acos : Inverse Cosine Function
+        7. ln : Natural Logarithm           16. atan : Inverse Tangent Function
+        8. log : Logarithm with Base
+        9. e : Euler's Constant
+        Note: (x) is the number you want to input.
+        """)
     user_input = input(f"{Color.yellow + Color.bold}Enter Equation Here >>> {Color.end}")
     
     if user_input == "Break":
